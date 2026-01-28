2ae502671dd4a699415c377b5edc09c48553104b
AHMAD KHAN <116090431+PRIMEUT@users.noreply.github.com>
2026-01-28 16:06:54 +0530

    Add files via upload

--- server.js
+++ server.js
@@ -1,3 +1,8 @@
+app.get("/", (req, res) => {
+    res.send("Server is running");
+});
+
+
 const express = require("express");
 const nodemailer = require("nodemailer");
 const cors = require("cors");
@@ -32,7 +37,7 @@ Message: ${message}
 
     res.json({ success:true });
 });
-
-app.listen(5000, ()=>{
-    console.log("Server running on port 5000");
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, ()=>{
+    console.log(`Server running on PORT ${PORT}`);
 });
